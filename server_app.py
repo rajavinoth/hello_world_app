fc8eb38aa21b78d532940c597ecd11ed225138e6
rajav <rajavinoth@2008@gmail.com>
2021-02-28 09:58:59 +0530

    initial commit hello world app

---
+++ server_app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+
+if __name__ == "__main__":
+    app.run(port=8050)
